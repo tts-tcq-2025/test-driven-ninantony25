c69a28171bd0c399bf8a8a61c3378b840efc4cdf
ninantony25 <57633711+ninantony25@users.noreply.github.com>
2025-10-10 08:56:25 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,9 +1,14 @@
 #include "StringCalculator.h"
 #include <string>
+#include <sstream>
 
 int StringCalculator::Add(const std::string& numbers) 
 {
     if (numbers.empty()) return 0;
 
-    return std::stoi(numbers); // handles single number input
+    std::stringstream ss(numbers);
+    int result = 0;
+    ss >> result;
+    return result;
 }
+
