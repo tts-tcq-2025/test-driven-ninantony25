5a186b32cd04f5f7653d4e6d2d7d8d62aa9d9e97
ninantony25 <57633711+ninantony25@users.noreply.github.com>
2025-10-10 09:04:13 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,9 +1,7 @@
 #include "StringCalculator.h"
 #include <string>
 #include <sstream>
-
-int StringCalculator::Add(const std::string& numbers) 
-{
+int StringCalculator::Add(const std::string& numbers) {
     if (numbers.empty()) return 0;
 
     std::stringstream ss(numbers);
