308419e3c80e93b5c36d9b3825404c1805328f07
ninantony25 <57633711+ninantony25@users.noreply.github.com>
2025-10-17 12:56:25 +0530

    Update string_test.cpp

--- string_test.cpp
+++ string_test.cpp
@@ -2,42 +2,36 @@
 #include <gtest/gtest.h>
 
 TEST(StringCalculatorTests, TC01_EmptyStringReturnsZero) {
-    StringCalculator calc;
-    EXPECT_EQ(calc.Add(""), 0);
+  StringCalculator calc;
+  EXPECT_EQ(calc.Add(""), 0);
 }
 
 TEST(StringCalculatorTests, TC02_SingleNumberInput) {
-    StringCalculator calc;
-    EXPECT_EQ(calc.Add("1"), 1);
+  StringCalculator calc;
+  EXPECT_EQ(calc.Add("1"), 1);
 }
 
 TEST(StringCalculatorTests, TC03_MultipleCommaSeparatedNumbers) {
-    StringCalculator calc;
-    EXPECT_EQ(calc.Add("1,2,3"), 6);
+  StringCalculator calc;
+  EXPECT_EQ(calc.Add("1,2,3"), 6);
 }
 
 TEST(StringCalculatorTests, TC04_NewlineAsValidDelimiter) {
-    StringCalculator calc;
-    EXPECT_EQ(calc.Add("1\n2,3"), 6);
+  StringCalculator calc;
+  EXPECT_EQ(calc.Add("1\n2,3"), 6);
 }
-
 
 TEST(StringCalculatorTests, TC05_CustomSingleCharacterDelimiter) {
-    StringCalculator calc;
-    EXPECT_EQ(calc.Add("//;\n1;2"), 3);
+  StringCalculator calc;
+  EXPECT_EQ(calc.Add("//;\n1;2"), 3);
 }
 
 TEST(StringCalculatorTests, TC06_CustomMultiCharacterDelimiter) {
-    StringCalculator calc;
-    EXPECT_EQ(calc.Add("//[***]\n1***2***3"), 6);
+  StringCalculator calc;
+  EXPECT_EQ(calc.Add("//[***]\n1***2***3"), 6);
 }
 
-TEST(StringCalculatorTests, TC06_CustomMultiCharacterDelimiter) {
-    StringCalculator calc;
-    EXPECT_EQ(calc.Add("//[***]\n1***2***3"), 6);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
